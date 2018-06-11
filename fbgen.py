7462152c4b395358299342f608e0765aea35ae3d
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-06-11 18:43:17 +0800

    Update fbgen.py

--- fbgen.py
+++ fbgen.py
@@ -1,2 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
+"""
+Utility script to generate/modify Firebreath plug-in projects.
+Original Author(s): Ben Loveridge, Richard Bateman
+Created:    14 December 2009
+License:    Dual license model; choose one of two:
