6aa4abe116728c3c5fb4e11545890d0d443a4ba5
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-06-11 18:45:01 +0800

    Update fbgen.py

--- fbgen.py
+++ fbgen.py
@@ -13,3 +13,16 @@ def getTemplateFiles(basePath, origPath=None):
     @param origPath used to strip path information from the returned values. Defaults to None.
     @returns array of strings each entry representing a single file.
     """
+  if origPath is None:
+        origPath = basePath
+    plen = len(origPath) + len(os.path.sep)
+    files = []
+    for filename in os.listdir(basePath):
+        tmpName = os.path.join(basePath, filename)
+        if filename[0:1] == '.' or tmpName is None:
+            continue
+        if os.path.isdir(tmpName):
+            files.extend(getTemplateFiles(tmpName, origPath) )
+        else:
+            files.append(tmpName[plen:])
+    return files
