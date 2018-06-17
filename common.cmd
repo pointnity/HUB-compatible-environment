c8b19e884c790522c4970884b83daae3296e89cf
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-06-17 16:55:20 +0800

    Update common.cmd

--- common.cmd
+++ common.cmd
@@ -6,3 +6,10 @@ set RUN_FROM=%CD%
 set _SHIFTTOK=0
 
 set _NPARAM=%~1
+IF /I "%_NPARAM%"=="" goto defaults
+IF /I "%_NPARAM:~0,1%"=="-" goto defaults
+set PROJDIR=%1
+shift
+set _SHIFTTOK=1
+
+set _NPARAM=%~1
